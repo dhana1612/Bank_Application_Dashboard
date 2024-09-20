210dfd2b109253b0d31f9b9d238ddb85f2a08633
Morning Shift <Morning Shift@DESKTOP-5AB50OC>
2024-09-21 05:09:08 +0530

    Add project files.

---
+++ Bank_Application_Dashboard/Operation_Process.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Application_Dashboard
+{
+    public class Operation_Process
+    {
+
+        long current_Balance = 704;
+
+        public void operation_Process()
+        {
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome: " + userName);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Account Number:");
+                    long acc_no = Convert.ToInt64(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("*** Enter a number, That was not a number, please try again ***");
+              
+                }
+
+            }
+
+              
+                bool continueOperation = true;
+                while (continueOperation)
+                {
+                    Console.WriteLine("Please select a option that you want to perform here:");
+
+                    Console.WriteLine(" 1.Current Balance \n 2.Deposit  \n 3.Withdraw \n 4.Exit");
+
+                    int option = Convert.ToInt16(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Your Current Balance: " + current_Balance);
+                            break;
+
+                        case 2:
+                            Console.Write("Enter the amount that you want to deposit: ");
+                            long deposit = Convert.ToInt64(Console.ReadLine());
+
+                            if (deposit != 0)//100
+                            {
+                                current_Balance = deposit + current_Balance;
+                                Console.WriteLine("Amount Deposited Successfully");
+                                Console.WriteLine("Updated Balance: " + current_Balance);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Deposited Amount");
+                            }
+
+                            break;
+
+                        case 3:
+                            Console.Write("Enter the amount that you want to withdraw: ");
+                            int withdraw = Convert.ToInt16(Console.ReadLine());
+
+                            if (withdraw < current_Balance)
+                            {
+                                current_Balance = current_Balance - withdraw;
+                                Console.WriteLine("Amount Withdraw Successfully");
+                                Console.WriteLine("Current Balance: " + current_Balance);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Insufficient Balance");
+                            }
+
+                            break;
+                        case 4:
+                            Console.WriteLine("Thank you");
+                            continueOperation = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("oops! Please select a correct option");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
---
+++ Bank_Application_Dashboard/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Bank_Application_Dashboard;
+using System;
+
+namespace BankApplication
+{
+    class BankApplication
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("\n\tWelcome to Abc Bank SYSTEM");
+            Console.WriteLine("----------------------------------------");
+
+            Operation_Process op = new Operation_Process();
+
+            op.operation_Process();
+
+        }
+    }
+}
